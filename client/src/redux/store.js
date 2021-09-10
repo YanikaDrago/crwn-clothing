520d0669908b7985ba264c827cb66df92293d8de
Iana Dragomir <yanadragomir@mail.ru>
2021-09-10 21:34:55 +0300

    add redux-devtools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 // import thunk from 'redux-thunk';
@@ -15,7 +15,11 @@ if (process.env.NODE_ENV === 'development') {  // записи logger в console
     middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(
+        rootReducer, 
+        composeEnhancers(applyMiddleware(...middlewares))
+    );
 
 sagaMiddleware.run(rootSaga);
 
